5fa6ffeafb39fee8a23d88d83efed90a7ed00300
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-14 01:00:03 +0100

    Connect HSQL

--- ConnectHSQLDB/src/com/lynda/javatraining/db/Main.java
+++ ConnectHSQLDB/src/com/lynda/javatraining/db/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	private static final String USERNAME = "dbuser";
 	private static final String PASSWORD = "dbpassword";
 	private static final String CONN_STRING =
-			"jdbc:mysql://localhost/explorecalifornia";
+			"jdbc:hsqldb:data/explorecalifornia";
 	
 	public static void main(String[] args) throws SQLException {
 		
