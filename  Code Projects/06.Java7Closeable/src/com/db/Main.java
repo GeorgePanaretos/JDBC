e0d4787ba013912806371e9ccd5f0094ba1afa03
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-18 00:48:35 +0100

    Java 7 Closeable

---  Code Projects/06.Java7Closeable/src/com/db/Main.java
+++  Code Projects/06.Java7Closeable/src/com/db/Main.java
@@ -9,32 +9,20 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = DBUtil.getConnection(DBType.HSQLDB);
-			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-			rs = stmt.executeQuery("SELECT stateId, stateName FROM states");
-			
+		try (
+
+				Connection conn = DBUtil.getConnection(DBType.HSQLDB);
+				Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+				ResultSet rs = stmt.executeQuery("SELECT stateId, stateName FROM states");
+				){
 			rs.last();
 			System.out.println("Number of rows: " + rs.getRow());
 			
 		} catch (SQLException e) {
 			DBUtil.processException(e);
-		} finally {
-			if (rs != null) {
-				rs.close();
-			}
-			if (stmt != null) {
-				stmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
+		} 
 		}
 		
 	}
 
-}
+
