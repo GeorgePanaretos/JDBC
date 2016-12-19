2f1b29557276c5c385e334bd0b9e4fd1fdeb4c33
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-19 03:11:38 +0100

    Looping Rows

---  Code Projects/07.LoopingRows/src/com/db/Main.java
+++  Code Projects/07.LoopingRows/src/com/db/Main.java
@@ -5,8 +5,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.db.tables.Tours;
+/**
+ * 1.each instance of ResultSet has an in-memory cursor
+ * 2.the starting cursor position is before the first row of data
+ * 3.the most common operation is to move forward row to row
+ * ResultSet rs=stmt.executeQuery("SELECT * FROM states");
+ * while (rs.next()){
+ * 	String columnValue =rs.getString("columnName");
+ * 	OR
+ * 	String columnValue =rs.getString(column position);
+ * 	System.out.println(columnValue);
+ *  }
+ *  
+ * @author george156200911
+ *
+ */
 public class Main {
-
+/**
+ * 
+ * @param args
+ * @throws SQLException
+ */
 	public static void main(String[] args) throws SQLException {
 
 		try (
@@ -15,6 +35,8 @@ public class Main {
 						ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 				ResultSet rs = stmt.executeQuery("SELECT * FROM tours");
 				) {
+			
+			Tours.displayData(rs);
 			
 		} catch (SQLException e) {
 			DBUtil.processException(e);
