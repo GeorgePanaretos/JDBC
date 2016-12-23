f424a45c1c96bf54ef8136bb4c119b136749325a
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-23 23:26:57 +0100

    Limiting Rows

---  Code Projects/09.LimitingRows/src/com/db/Main.java
+++  Code Projects/09.LimitingRows/src/com/db/Main.java
@@ -11,13 +11,31 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
+//		ResultSet rs=null;
+//		try (
+//				Connection conn = DBUtil.getConnection(DBType.HSQLDB);
+//				Statement stmt = conn.createStatement();
+//				) {
+//			stmt.setMaxRows(5);
+//			rs = stmt.executeQuery(
+//					"SELECT tourId, tourName, price FROM tours");
+//			Tours.displayData(rs);
+//		} catch (SQLException e) {
+//			System.err.println(e);
+//		}
+//		finally{
+//			rs.close(); 
+//		}
+		
+		//ResultSet rs=null;
 		try (
 				Connection conn = DBUtil.getConnection(DBType.HSQLDB);
 				Statement stmt = conn.createStatement();
-				stmt.setMaxRows(5);
 				ResultSet rs = stmt.executeQuery(
-						"SELECT tourId, tourName, price FROM tours");
+						"SELECT tourId, tourName, price FROM tours "+
+						"LIMIT 0, 5");
 				) {
+			//stmt.setMaxRows(5);
 			Tours.displayData(rs);
 		} catch (SQLException e) {
 			System.err.println(e);
