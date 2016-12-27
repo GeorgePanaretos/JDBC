ef793a4eb8822f84371adbafe3058d0d00dc9a25
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-28 00:35:09 +0100

    Prepared Statements

---  Code Projects/10.PreparedStatements/src/com/db/Main.java
+++  Code Projects/10.PreparedStatements/src/com/db/Main.java
@@ -1,27 +1,37 @@
 package com.db;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import com.util.*;
 import com.db.tables.Tours;
 
 public class Main {
 
 	private static final String SQL = 
-			"SELECT tourId, tourName, price FROM tours";
+			"SELECT tourId, tourName, price FROM tours WHERE price <= ?";
 
 	public static void main(String[] args) throws Exception {
 
+		double maxPrice;
+		try{
+			maxPrice =InputHelper.getDoubleInput("Enter a maximum price: ");
+		}catch(NumberFormatException e){
+			System.err.println("Error: invalid number");
+			return;
+		}
 		ResultSet rs = null;
 		try (
 				Connection conn = DBUtil.getConnection(DBType.MYSQL);
-				Statement stmt = conn.createStatement(
+				PreparedStatement stmt = conn.prepareStatement(
+						SQL,
 						ResultSet.TYPE_SCROLL_INSENSITIVE, 
 						ResultSet.CONCUR_READ_ONLY);
 				) {
-			rs = stmt.executeQuery(SQL);
+			stmt.setDouble(1, maxPrice);
+			rs = stmt.executeQuery();
 			Tours.displayData(rs);
 
 		} catch (SQLException e) {
