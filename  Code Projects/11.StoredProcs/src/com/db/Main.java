988fdebb5b7690b1dec3f6ba68d894fecac881fe
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-31 20:06:06 +0100

    Stored Procs

---  Code Projects/11.StoredProcs/src/com/db/Main.java
+++  Code Projects/11.StoredProcs/src/com/db/Main.java
@@ -1,17 +1,17 @@
-package com.lynda.javatraining.db;
+package com.db;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.lynda.javatraining.db.tables.Tours;
-import com.lynda.javatraining.util.InputHelper;
+import com.db.tables.Tours;
+import com.util.InputHelper;
 
 public class Main {
-
-	private static final String SQL = 
-			"SELECT tourId, tourName, price FROM tours WHERE price <= ?";
+	// call of stored procs
+	private static final String SQL = "{call GetToursByPrice(?)}";
 
 	public static void main(String[] args) throws Exception {
 
@@ -26,7 +26,7 @@ public class Main {
 		ResultSet rs = null;
 		try (
 				Connection conn = DBUtil.getConnection(DBType.MYSQL);
-				PreparedStatement stmt = conn.prepareStatement(
+				CallableStatement stmt = conn.prepareCall(
 						SQL,
 						ResultSet.TYPE_SCROLL_INSENSITIVE, 
 						ResultSet.CONCUR_READ_ONLY);
